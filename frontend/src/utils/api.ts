65e441f93c6ca281bf00e075f1e020969ac05d36
Krish Shah <krishcshah09@gmail.com>
2026-06-26 13:54:09 +0530

    moved CreateResume to ResumeForm component for reusibility in edit resume form

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import type {PostResumeType} from '../pages/CreateResume'
+import type {PostResumeType} from '../components/ResumeForm'
 
 const api=axios.create({
     baseURL:import.meta.env.VITE_BACKEND_URL||'http://localhost:8000',
