145c4be64d8a488fe83f955421be77440043c0ba
Krish Shah <krishcshah09@gmail.com>
2026-06-26 10:36:03 +0530

    section 2 complete

--- frontend/src/pages/CreateResume.tsx
+++ frontend/src/pages/CreateResume.tsx
@@ -1,5 +1,5 @@
 import './CreateResume.css'
-import {useForm} from 'react-hook-form'
+import {useFieldArray,useForm} from 'react-hook-form'
 import type { SubmitHandler } from 'react-hook-form'
 import * as z from 'zod'
 import {zodResolver} from '@hookform/resolvers/zod'
@@ -74,58 +74,98 @@ export default function CreateResume(){
     const [section,setSection]=useState<1|2|3|4|5>(1)
     const [skillCurrent,setSkillCurrent]=useState<string>('')
 
-    const {register,handleSubmit,formState:{errors},setValue,watch}=useForm({
+    const {control,register,handleSubmit,formState:{errors},setValue,watch}=useForm({
         resolver:zodResolver(ResumeSchema),
         defaultValues:{
             skills:[],
             visibility:"true"
         }
     })
+    const educationField=useFieldArray({
+        control,
+        name:'education'
+    })
     const onSubmit:SubmitHandler<InputType>=(data)=>console.log(data)
     const skills=watch('skills')
     const visibility=watch('visibility')
 
     return(
-        <form onSubmit={handleSubmit(onSubmit)}>
-            {section===1?
-            <div>
-                Title:<input {...register('title')} required={true}/>
-                {errors.title?<p>{errors.title.message}</p>:null}
-                Summary:<input {...register('summary')} required={true}/>
-                {errors.summary?<p>{errors.summary.message}</p>:null}
-                Skills:<div>
-                    {(skills||[]).map((skill,index)=>{
-                        return(
-                            <div key={index}>{skill}<button type="button" onClick={()=>{
-                                setValue('skills',(skills||[]).filter((_,i)=>i!==index))
-                            }}>🗑️</button></div>
-                        )
-                    })}
-                    <input type="text" value={skillCurrent} onChange={(e)=>setSkillCurrent(e.target.value)}/>
-                    <button type="button" onClick={()=>{
-                        setValue('skills',[...(skills||[]),skillCurrent])
-                        setSkillCurrent('')
-                    }}>+</button>
+        <div>
+            <button onClick={()=>setSection(1)}>Primary</button>
+            <button onClick={()=>setSection(2)}>Education</button>
+            <button onClick={()=>setSection(3)}>Experience</button>
+            <button onClick={()=>setSection(4)}>Projects</button>
+            <button onClick={()=>setSection(5)}>Achievements</button>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                {section===1?
+                <div>
+                    Title:<input {...register('title')} required={true}/>
+                    {errors.title?<p>{errors.title.message}</p>:null}
+                    Summary:<input {...register('summary')} required={true}/>
+                    {errors.summary?<p>{errors.summary.message}</p>:null}
+                    Skills:<div>
+                        {(skills||[]).map((skill,index)=>{
+                            return(
+                                <div key={index}>{skill}<button type="button" onClick={()=>{
+                                    setValue('skills',(skills||[]).filter((_,i)=>i!==index))
+                                }}>🗑️</button></div>
+                            )
+                        })}
+                        <input type="text" value={skillCurrent} onChange={(e)=>setSkillCurrent(e.target.value)}/>
+                        <button type="button" onClick={()=>{
+                            if(skillCurrent===''){
+                                return
+                            }
+                            setValue('skills',[...(skills||[]),skillCurrent])
+                            setSkillCurrent('')
+                        }}>+</button>
+                    </div>
+                    {errors.skills?<p>{errors.skills.message}</p>:null}
+                    Linkedin(optional):<input type="url" {...register('linkedin',{
+                        setValueAs:(value)=>value===""?undefined:value
+                    })}/>
+                    {errors.linkedin?<p>{errors.linkedin.message}</p>:null}
+                    Visibility:
+                    <input type="radio" style={{display:"none"}} id="visibility-true"value="true"{...register('visibility')}/>
+                    <input type="radio" style={{display:"none"}} id="visibility-false"value="false"{...register('visibility')}/>
+                    <label htmlFor="visibility-true" className={visibility==='true'?'visbility-selected':''}>Public</label>
+                    <label htmlFor="visibility-false" className={visibility==='false'?'visbility-selected':''}>Private</label>
+                    {errors.visibility?<p>{errors.visibility.message}</p>:null}
                 </div>
-                {errors.skills?<p>{errors.skills.message}</p>:null}
-                Linkedin(optional):<input type="url" {...register('linkedin',{
-                    setValueAs:(value)=>value===""?undefined:value
-                })}/>
-                {errors.linkedin?<p>{errors.linkedin.message}</p>:null}
-                Visibility:
-                <input type="radio" style={{display:"none"}} id="visibility-true"value="true"{...register('visibility')}/>
-                <input type="radio" style={{display:"none"}} id="visibility-false"value="false"{...register('visibility')}/>
-                <label htmlFor="visibility-true" className={visibility==='true'?'visbility-selected':''}>Public</label>
-                <label htmlFor="visibility-false" className={visibility==='false'?'visbility-selected':''}>Private</label>
-                {errors.visibility?<p>{errors.visibility.message}</p>:null}
-            </div>
-            :
-            null}
-            {section===2?<div></div>:null}
-            {section===3?<div></div>:null}
-            {section===4?<div></div>:null}
-            {section===5?<div></div>:null}
-            <button>Save</button>
-        </form>
+                :
+                null}
+                {section===2?
+                <div>
+                    {educationField.fields.map((field,index)=>(
+                        <div key={field.id}>
+                            <input {...register(`education.${index}.institution`)}/>
+                            <p>{errors.education?.[index]?.institution?.message||null}</p>
+                            <input {...register(`education.${index}.degree`)}/>
+                            <p>{errors.education?.[index]?.degree?.message||null}</p>
+                            <input type="date" {...register(`education.${index}.startDate`)}/>
+                            <p>{errors.education?.[index]?.startDate?.message}</p>
+                            <input type="date" {...register(`education.${index}.endDate`,{
+                                setValueAs:(val)=>val===''?undefined:val
+                            })}/>
+                            <p>{errors.education?.[index]?.endDate?.message}</p>
+                            <button type="button" onClick={()=>educationField.remove(index)}>🗑️</button>
+                            <p>{errors.education?.[index]?.message}</p>
+                        </div>
+                    ))}
+                    <button type="button" onClick={()=>educationField.append({
+                        institution:'',
+                        degree:'',
+                        startDate:'',
+                        endDate:''
+                    })}>+</button>
+                </div>
+                :
+                null}
+                {section===3?<div></div>:null}
+                {section===4?<div></div>:null}
+                {section===5?<div></div>:null}
+                <button>Save</button>
+            </form>
+        </div>
     )
 }
