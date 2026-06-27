7699dde48aa4c65a5d1b0333ca68fe55847eb165
Krish Shah <krishcshah09@gmail.com>
2026-06-27 17:47:05 +0530

    fixed cors issue. V1 finally completegit add .git add .git add .git add .git add .

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,7 +12,7 @@ dotenv.config()
 const app=express()
 app.use(express.json())
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:process.env.FRONTEND_URL||'http://localhost:5173',
     credentials:true
 }))
 app.use(cookieParser())
