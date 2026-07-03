c09896aa4e1ff7e412cc0177e26f7070405627e2
Krish Shah <krishcshah09@gmail.com>
2026-07-03 18:24:02 +0530

    added socket.io-client and created context with provider for it

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import {QueryClient,QueryClientProvider} from '@tanstack/react-query'
 import {Toaster} from 'react-hot-toast'
+import { SocketProvider } from './utils/SocketContext.tsx'
 
 const queryClient = new QueryClient()
 
@@ -11,7 +12,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Toaster/>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <SocketProvider>
+        <App />
+      </SocketProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
