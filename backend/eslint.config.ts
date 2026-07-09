eb736041c283345e59306e5d7b17438939677de9
Krish Shah <krishcshah09@gmail.com>
2026-07-09 14:51:52 +0530

    final prettier fixes

--- backend/eslint.config.ts
+++ backend/eslint.config.ts
@@ -4,7 +4,7 @@ import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  {ignores: ["dist/**", "generated/**", "node_modules/**"]},
+  { ignores: ["dist/**", "generated/**", "node_modules/**"] },
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
